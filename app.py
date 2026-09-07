2df231a64a36040a634358cc6dc8856ee7199cf5
ma3wahee <ma3wahee@uwaterloo.ca>
2026-09-07 08:36:32 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ def particle_movement():
     zero = 0 
     right = zero + 1
     left = zero - 1
+    #possible moves: left, right, or stay at current position.
     moves = [zero, left, right]
+    #simply to moves = [-1,0,1] in phases 2-4 indicate this by *
+    #randomly choose a move from the given moves.
     random_move = random.choice(moves)
     return(random_move)
 
@@ -14,10 +17,10 @@ print(particle_movement())
 #phase 2
 def repeat_movement():
     for step in range(5):
-       position = 0 
-       right = position + 1
-       left = position - 1
-       moves = [position, left, right]
+       #possible moves: left, right, or stay at current position.
+       #*
+       moves = [-1,0,1]
+       #randomly choose a move from the given moves.
        random_move = random.choice(moves)
        print(random_move,step)
 
@@ -27,10 +30,10 @@ repeat_movement()
 def store_moves():
     store =[]
     for step in range (5):
-        position = 0 
-        right = position + 1
-        left = position - 1
-        moves = [position, left, right]
+        #possible moves: left, right, or stay at current position. 
+        #*
+        moves = [-1,0,1]
+        #randomly choose a move from the given moves.
         random_move = random.choice(moves)
         store.append (random_move)
         print(store,step)  
@@ -39,18 +42,22 @@ store_moves()
 
 #phase 4
 def walk():
+         #starts the particle trajectory at the orgin.
          walk_store =[0]
-         current_new = 0
+         current_position = 0
          for step in range(100):
-            position = 0 
-            right = position + 1
-            left = position - 1
-            moves = [position, left, right]
+            #possible moves: left, right, or stay at current position.
+            #* 
+            moves = [-1,0,1]
+            #randomly choose a move from the given moves.
             random_move = random.choice(moves)
-            new_position = current_new + random_move
+            #calculate particle's next position.
+            new_position = current_position + random_move
+            #record particle's new position in particle's path.
             walk_store.append(new_position)
             #print("step =",step,"length =",len(walk_store))
-            current_new = walk_store[-1]
+            #update current position for the next step
+            current_position = walk_store[-1]
             print(walk_store)
 
 
